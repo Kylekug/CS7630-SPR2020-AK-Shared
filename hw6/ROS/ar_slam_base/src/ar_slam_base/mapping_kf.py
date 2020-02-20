5f9a73d2dc56c4607adfd15b90701420e0d4fde6
BATES <kbates@gtlpc101.georgiatech-metz.fr>
2020-02-20 16:07:29 +0100

    hw6 complete (sans turtlebot) - note polar not xyz; future issues?

--- hw6/ROS/ar_slam_base/src/ar_slam_base/mapping_kf.py
+++ hw6/ROS/ar_slam_base/src/ar_slam_base/mapping_kf.py
@@ -2,14 +2,14 @@ import roslib; roslib.load_manifest('ar_mapping_base')
 import rospy
 from numpy import *
 from numpy.linalg import inv
-from math import pi, sin, cos
+from math import pi, sin, cos, sqrt
 from visualization_msgs.msg import Marker, MarkerArray
 from geometry_msgs.msg import Point, Pose, PoseStamped
 import tf
 import threading
 
-import rover_driver
-from rover_driver.rover_kinematics import *
+import rover_driver_base
+from rover_kinematics import *
 
 
 
@@ -49,12 +49,23 @@ class MappingKF(RoverKinematics):
         Rtheta = mat([[cos(theta), -sin(theta), 0], 
                       [sin(theta),  cos(theta), 0],
                       [         0,           0, 1]]);
-        DeltaX = iW*S
+        dX = iW*S
         # Update the state using odometry (same code as for the localisation
         # homework), but we only need to deal with a subset of the state:
         # TODO
         # self.X[0:3,0] = ...
         # self.P[0:3,0:3] = ...
+        
+        #KB to Ayoub: I just c/p'd your code from rover_kf.py,
+        #i.e. RoverKF.predict (above code sets up same Rtheta)
+        self.X[0:3,0] = self.X[0:3,0] + Rtheta*dX
+        A = mat([[1, 0, -sin(theta)*dX[0,0]-cos(theta)*dX[1,0]],
+                 [0, 1,  cos(theta)*dX[0,0]-sin(theta)*dX[1,0]],
+                 [0, 0,                       1              ]])
+        B = Rtheta*iW
+        Qu = mat(diag([encoder_precision**2]*len(S)))
+        self.P[0:3,0:3] = A * self.P[0:3,0:3] * A.T + B * Qu * B.T
+        
         self.lock.release()
         return (self.X,self.P)
 
@@ -68,6 +79,93 @@ class MappingKF(RoverKinematics):
         # TODO
         # self.X = ...
         # self.P = ...
+        
+        #not mentioned here is that "self.X" will now include landmarks
+        #appended to the end as we find them
+        #i.e. first 3 rows will be X, second 3 rows will be the first
+        #landmark we encounter, and so on:
+        
+        #FIX ME
+        # if Id not in self.idx.keys(): #don't need ".keys()" here, but explicit!
+            # self.idx[Id] = len(self.X) / 3
+            # self.X = vstack([self.X, L])
+            # self.P = vstack([
+        # else:
+        
+        
+        #following:
+        #http://ais.informatik.uni-freiburg.de/teaching/ws12/mapping/pdf/slam04-ekf-slam.pdf
+        #(which uses polar)
+        
+        
+        Id = id
+        
+        #convert Z "x and y" to "r and psi" (both in robo frame)
+        Z_polar = mat(vstack([linalg.norm(Z),arctan2(Z[1,0],Z[0,0])]))
+        
+        R = mat(diag([uncertainty**2,0.1**2])) #TODO: is this polar equivalent ok? :(
+        
+        #R = mat(diag([uncertainty, uncertainty]))
+        
+        print("Z_polar = ", Z_polar)
+        
+        if Id not in self.idx.keys():
+            self.idx[Id] = 3 + (2*len(self.idx))
+            Z_prev = mat(vstack([
+                self.X[0,0] + Z_polar[0,0] * cos(Z_polar[1,0]+self.X[2,0]),
+                self.X[1,0] + Z_polar[0,0] * sin(Z_polar[1,0]+self.X[2,0])
+                ]))
+            self.X = mat(vstack([self.X, Z_prev]))
+            
+            Px = size(self.P,0)
+            Py = size(self.P,1)
+            newP = zeros((Px+2,Py+2))
+            newP[0:Px,0:Py] = self.P
+            newP[Px:,Py:] = R
+            self.P = newP
+            
+            print("New P!")
+            print(newP)
+        
+        xId = self.idx[Id]
+        
+        print("xId = ", xId)
+        print("self.X[xId:xId+2,0] = ", self.X[xId:xId+2,0])
+        print("self.X = ", self.X)
+        
+        Z_prev = mat(vstack(self.X[xId:xId+2,0]))
+        
+        print("Z_prev = ", Z_prev)
+    
+        delta = Z_prev - self.X[0:2,0]
+        
+        q = delta.T * delta
+        
+        q = q[0,0]
+
+        
+        #"Z_estimate"
+        Z_e = mat(vstack([sqrt(q),atan2(delta[1,0],delta[0,0]) - self.X[2,0]]))
+        
+        j = (xId - 1) / 2
+        
+        F = zeros((5,len(self.X)))
+        F[0:3,0:3] = identity(3)
+        F[3:5,3+(2*j-2):3+(2*j-2)+2] = identity(2)
+        
+        print("F = ", F)
+        
+        H_mini = multiply(1/q, mat([[-sqrt(q) * delta[0,0], -sqrt(q) * delta[1,0], 0, sqrt(q) * delta[0,0], sqrt(q) * delta[1,0]],
+                      [delta[1,0], -delta[0,0], -q, -delta[1,0], delta[0,0]]])  )
+
+        
+        H = H_mini * F
+        K = self.P * H.T * inv(H * self.P * H.T + R)
+        self.X = self.X + K * (Z - Z_e)
+        #self.P = (mat(eye(3)) - K * H) * self.P
+        self.P = (identity(size(self.P,0)) - K*H) * self.P
+        
+        
         self.lock.release()
         return (self.X,self.P)
 
@@ -128,8 +226,12 @@ class MappingKF(RoverKinematics):
             marker.pose.orientation.y = 0
             marker.pose.orientation.z = 1
             marker.pose.orientation.w = 0
+            #default assumes x and y on diagonal for covariance,
+            #marker.scale.x = 3*sqrt(self.P[l,l])
+            #marker.scale.y = 3*sqrt(self.P[l+1,l+1]);
+            #however we are unfortunately using radius/phi:
             marker.scale.x = 3*sqrt(self.P[l,l])
-            marker.scale.y = 3*sqrt(self.P[l+1,l+1]);
+            marker.scale.y = 3*sqrt(self.P[l,l])
             marker.scale.z = 0.1;
             marker.color.a = 1.0;
             marker.color.r = 1.0;
